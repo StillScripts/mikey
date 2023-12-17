4c247869915f31604eea9893be5049d98f816d0c
StillScripts <80142739+StillScripts@users.noreply.github.com>
2023-12-17 14:38:07 +1100

    Add dark mode and fix UI issues

--- app/(dashboard)/_components/desktop-sidebar.tsx
+++ app/(dashboard)/_components/desktop-sidebar.tsx
@@ -3,18 +3,18 @@
 import { SidebarNavigation } from './sidebar-navigation'
 
 export const DesktopSidebar = () => {
-  return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-      <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-        <div className="flex h-16 shrink-0 items-center">
-          <img
-            className="h-8 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-        </div>
-        <SidebarNavigation />
-      </div>
-    </div>
-  )
+	return (
+		<div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+			<div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-muted px-6 pb-4">
+				<div className="flex h-16 shrink-0 items-center">
+					<img
+						className="h-8 w-auto"
+						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+						alt="Your Company"
+					/>
+				</div>
+				<SidebarNavigation />
+			</div>
+		</div>
+	)
 }
--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -12,13 +12,13 @@ export const metadata: Metadata = {
 const PostsPage = async () => {
 	const allPosts = await getAllPosts()
 	return (
-		<div className="bg-white py-24 sm:py-32">
+		<div className="bg-background py-24 sm:py-32">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
 				<div className="mx-auto max-w-2xl text-center">
-					<h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+					<h2 className="text-3xl font-bold tracking-tight text-foreground sm:text-4xl">
 						From the blog
 					</h2>
-					<p className="mt-2 text-lg leading-8 text-gray-600">
+					<p className="mt-2 text-lg leading-8 text-muted-foreground">
 						Learn how to grow your business with our expert advice.
 					</p>
 				</div>
