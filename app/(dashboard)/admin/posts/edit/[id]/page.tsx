9e0a21152817b09f41fd8ff326eb2ea20c680a5b
StillScripts <80142739+StillScripts@users.noreply.github.com>
2023-12-30 10:40:10 +1100

    Enable selecting editor based on searchParams

--- app/(dashboard)/admin/posts/edit/[id]/page.tsx
+++ app/(dashboard)/admin/posts/edit/[id]/page.tsx
@@ -2,15 +2,25 @@ import { notFound } from 'next/navigation'
 
 import { getPost } from '@/app/(server)/routers/posts'
 
+import { EditPostForm as EditPostEditorJs } from './editor-js-form'
 import { EditPostForm } from './form'
 
 export const dynamic = 'force-dynamic'
 
-const EditPost = async ({ params }: { params: { id: string } }) => {
+type Params = {
+	params: { id: string }
+	searchParams: { [key: string]: string | undefined }
+}
+
+const EditPost = async ({ params, searchParams }: Params) => {
 	const posts = await getPost(parseInt(params.id))
 	const post = posts ? posts[0] : null
 	if (!post) {
 		notFound()
+	}
+
+	if (searchParams?.editor === 'editor-js') {
+		return <EditPostEditorJs post={post} />
 	}
 
 	return <EditPostForm post={post} />
