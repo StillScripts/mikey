eeee4d6f2891e447bd1d4a57eb35de468aaeb76a
StillScripts <80142739+StillScripts@users.noreply.github.com>
2024-01-01 13:26:04 +1100

    Add exercise tables

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,7 @@
 import { sql } from 'drizzle-orm'
 import {
 	boolean,
+	int,
 	json,
 	mysqlEnum,
 	mysqlTable,
@@ -32,4 +33,24 @@ export const blocks = mysqlTable('blocks', {
 	type: mysqlEnum('type', ['cards']),
 	title: varchar('title', { length: 255 }).notNull(),
 	content: json('content')
+})
+
+export const exerciseSession = mysqlTable('exercise_session', {
+	id: serial('id').primaryKey(),
+	date: timestamp('date').notNull(),
+	notes: text('notes')
+})
+
+// TODO, add relationship where 1 session has many sets
+
+export const exerciseSet = mysqlTable('exercise_set', {
+	id: serial('id').primaryKey(),
+	title: varchar('title', { length: 255 }).notNull(),
+	reps: int('reps'),
+	createdAt: timestamp('created_at')
+		.default(sql`CURRENT_TIMESTAMP`)
+		.notNull(),
+	updatedAt: timestamp('updated_at')
+		.default(sql`CURRENT_TIMESTAMP`)
+		.notNull()
 })
